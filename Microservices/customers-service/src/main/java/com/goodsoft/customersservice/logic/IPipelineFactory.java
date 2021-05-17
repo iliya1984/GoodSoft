1aeaa574b539e6cc95a4524c9b94237c411424a4
iliya1984 <55304922+iliya1984@users.noreply.github.com>
2021-05-17 22:18:48 +0300

    Added custom validators

--- Microservices/customers-service/src/main/java/com/goodsoft/customersservice/logic/IPipelineFactory.java
+++ Microservices/customers-service/src/main/java/com/goodsoft/customersservice/logic/IPipelineFactory.java
@@ -5,5 +5,5 @@ import an.awesome.pipelinr.Pipeline;
 
 public interface IPipelineFactory
 {
-    <R, C extends  Command<R>> Pipeline getPipeline(String requestName);
+    <R, C extends  Command<R>> Pipeline getPipeline(String requestName) throws IllegalArgumentException;
 }
--- Microservices/customers-service/src/main/java/com/goodsoft/customersservice/logic/PipelineFactory.java
+++ Microservices/customers-service/src/main/java/com/goodsoft/customersservice/logic/PipelineFactory.java
@@ -5,6 +5,7 @@ import an.awesome.pipelinr.Pipeline;
 import an.awesome.pipelinr.Pipelinr;
 import com.goodsoft.customersservice.CustomersServiceApplication;
 import com.goodsoft.customersservice.logic.annotations.CustomerRequestHanlder;
+import com.goodsoft.customersservice.logic.middlewares.ValidationMiddleware;
 import com.goodsoft.customersservice.logic.requests.getcustomer.GetCustomerRequestHandler;
 import org.apache.naming.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class PipelineFactory implements IPipelineFactory
@@ -28,13 +30,25 @@ public class PipelineFactory implements IPipelineFactory
 
     @Bean
     @Override
-    public <R, C extends  Command<R>> Pipeline getPipeline(String requestName) {
+    public <R, C extends  Command<R>> Pipeline getPipeline(String requestName) throws IllegalArgumentException {
 
+        var beanNames = context.getBeanNamesForAnnotation(CustomerRequestHanlder.class);
         var handlerName = requestName + "Handler";
-        var requestHandler = (Command.Handler<C, R>)context.getBean(handlerName);
 
-        var pipeline = new Pipelinr()
-                .with(() -> Stream.of(requestHandler) );
-        return pipeline;
+        if(Arrays.stream(beanNames).anyMatch(bn -> bn.equals(handlerName)))
+        {
+            var validator = new ValidationMiddleware<C>();
+            var requestHandler = (Command.Handler<C, R>)context.getBean(handlerName);
+
+            var pipeline = new Pipelinr()
+                    .with(() -> Stream.of(validator))
+                    .with(() -> Stream.of(requestHandler) );
+            return pipeline;
+        }
+        else
+        {
+            String errorMessage = "Error occurred. Request handler for " + requestName + " was not found";
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
