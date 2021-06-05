7f1733a7c05d84e980cf8c7fea042c1c0bb287a7
iliya1984 <55304922+iliya1984@users.noreply.github.com>
2021-06-05 11:29:52 +0300

    CustomerController restapi adjustments

--- Microservices/customers-service/src/main/java/com/goodsoft/customersservice/controllers/CustomerController.java
+++ Microservices/customers-service/src/main/java/com/goodsoft/customersservice/controllers/CustomerController.java
@@ -4,7 +4,6 @@ import com.goodsoft.customersservice.logic.createcustomer.CreateCustomerCommand;
 import com.goodsoft.customersservice.logic.getcustomer.GetCustomerQuery;
 import com.goodsoft.infra.mediator.errorhandling.ValidationException;
 import com.goodsoft.infra.mediator.factory.IPipelineFactory;
-import com.goodsoft.interfaces.customers.ICustomersService;
 import com.goodsoft.interfaces.customers.errorhandling.CustomersValidationError;
 import com.goodsoft.interfaces.customers.requestresponse.CreateCustomerRequest;
 import com.goodsoft.interfaces.customers.requestresponse.CreateCustomerResponse;
@@ -14,13 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/customers")
-public class CustomerController implements ICustomersService
+public class CustomerController //implements ICustomersService
 {
     private IPipelineFactory _pipelineFactory;
 
@@ -45,7 +41,8 @@ public class CustomerController implements ICustomersService
     }
 
     @PostMapping()
-    public ResponseEntity<CreateCustomerResponse> create(@RequestBody CreateCustomerRequest model)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> create(@RequestBody CreateCustomerRequest model)
     {
         try
         {
@@ -53,44 +50,40 @@ public class CustomerController implements ICustomersService
             request.setRequest(model);
 
             var pipeline = _pipelineFactory.getPipeline(request);
-
             var customer = pipeline.send(request);
 
-            var response = new CreateCustomerResponse();
-
-            //TODO: set customer
-            //response.setCustomer(customer);
-
-            return new ResponseEntity<CreateCustomerResponse>(response, HttpStatus.OK);
-
+            return new ResponseEntity(customer, HttpStatus.CREATED);
         }
         catch(Exception ex)
         {
-            var exceptionName = ex.getClass().getSimpleName();
-            if(exceptionName.equals("ValidationException"))
-            {
-                var validationException = (ValidationException)ex;
-
-                var invalidResponse = new CreateCustomerResponse();
-                var errors = new ArrayList<CustomersValidationError>();
-                validationException.getErrors().forEach(e ->
-                {
-                    var error = new CustomersValidationError();
-                    error.setErrorMessage(e.getMessage());
-                    error.setErrorCode(e.getCode());
-                    errors.add(error);
-                });
-
-                invalidResponse.setValidationErrors(errors);
-
-
-                return new ResponseEntity(invalidResponse, HttpStatus.UNPROCESSABLE_ENTITY);
-            }
-            else
-            {
-                return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+            return exceptionResponse(ex);
         }
+    }
 
+    private  ResponseEntity<?> exceptionResponse(Exception ex)
+    {
+        var exceptionName = ex.getClass().getSimpleName();
+        if(exceptionName.equals("ValidationException"))
+        {
+            var validationException = (ValidationException)ex;
+
+            var invalidResponse = new CreateCustomerResponse();
+            var errors = new ArrayList<CustomersValidationError>();
+            validationException.getErrors().forEach(e ->
+            {
+                var error = new CustomersValidationError();
+                error.setErrorMessage(e.getMessage());
+                error.setErrorCode(e.getCode());
+                errors.add(error);
+            });
+
+            invalidResponse.setValidationErrors(errors);
+
+            return new ResponseEntity(invalidResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        else
+        {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
