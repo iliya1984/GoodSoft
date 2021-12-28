4eb085879fbdefdb9c39f47e2e6d143e3d7274fa
iliya <iliya.nahshan@gmail.com>
2021-12-29 01:46:34 +0200

    Fixed customer email insert

--- Microservices/customers-service/src/main/java/com/goodsoft/customersservice/dal/implementations/CustomersDal.java
+++ Microservices/customers-service/src/main/java/com/goodsoft/customersservice/dal/implementations/CustomersDal.java
@@ -2,9 +2,13 @@ package com.goodsoft.customersservice.dal.implementations;
 
 import com.goodsoft.customersservice.configuration.CustomerServiceConfiguration;
 import com.goodsoft.customersservice.dal.abstractions.ICustomersDal;
+import com.goodsoft.customersservice.entities.CustomerEmailEntity;
 import com.goodsoft.customersservice.entities.CustomerEntity;
+import com.mongodb.DBObject;
 import org.springframework.stereotype.Service;
 import org.bson.Document;
+
+import java.util.ArrayList;
 
 @Service
 public class CustomersDal extends BaseDal implements ICustomersDal
@@ -27,6 +31,17 @@ public class CustomersDal extends BaseDal implements ICustomersDal
         document.append("customerId", customer.getId().toString());
         document.append("firstName", customer.getFirstName());
         document.append("lastName", customer.getLastName());
+
+        var emails = new ArrayList<Document>();
+
+        for (CustomerEmailEntity email : customer.getEmails())
+        {
+            var emailDocument = new Document();
+            emailDocument.append("email", email.getEmail());
+            emailDocument.append("isPrimary", email.isPrimary());
+            emails.add(emailDocument);
+        }
+        document.append("emails", emails);
 
         collection.insertOne(document);
 
