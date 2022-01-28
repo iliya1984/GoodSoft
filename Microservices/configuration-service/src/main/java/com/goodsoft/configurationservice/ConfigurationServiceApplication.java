14056cb8efdadb42744a031c7e36b844059f8a6d
iliya <iliya.nahshan@gmail.com>
2022-01-28 22:14:56 +0200

    Added tomcat deployment

--- Microservices/configuration-service/src/main/java/com/goodsoft/configurationservice/ConfigurationServiceApplication.java
+++ Microservices/configuration-service/src/main/java/com/goodsoft/configurationservice/ConfigurationServiceApplication.java
@@ -4,15 +4,22 @@ import com.goodsoft.configurationservice.logic.ConfigurationServiceManager;
 import com.goodsoft.configurationservice.logic.IConfigurationServiceManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ConfigurationServiceApplication {
+public class ConfigurationServiceApplication extends SpringBootServletInitializer {
 
 	//Mongo: connection string = mongodb://localhost:27017
 	public static void main(String[] args)
 	{
 		SpringApplication.run(ConfigurationServiceApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ConfigurationServiceApplication.class);
 	}
 
 	@Bean
